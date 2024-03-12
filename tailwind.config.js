487266d58c6a214f8b407112ee59f454daa7da25
Green Hornet <taphousecarl@gmail.com>
2024-03-12 19:02:16 -0400

    changing colors with tailwind cc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./**/*.{razor,html}"],
   theme: {
-    extend: {},
+    extend: {
+    colors: {
+      'bubble-gum': '#ff77e9',
+      'mountain-dew': '#ff77e9',
+      'crimson': '#dc143c'
+    }
   },
   plugins: [],
 }
-
+}
